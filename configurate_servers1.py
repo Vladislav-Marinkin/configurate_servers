5c3960dada667d16ffe165b94534a51a029c3523
Владислав <vladislav-marinkin@outlook.com>
2024-07-06 19:41:46 +0400

    fix errors

--- configurate_servers1.py
+++ configurate_servers1.py
@@ -6,19 +6,19 @@ from ARGParse import ARGParse
 
 if __name__ == "__main__":
     arg_parse = ARGParse()
+    user_dialog = UserDialog()
     if not arg_parse.parse():
         # Запрашиваем логин и пароль, ключ или указываем, что форма входа для каждого сервера будет разная
-        user_dialog = UserDialog()
         user_dialog.prompt_login_credentials()
-
-        # Сканируем сеть на наличие хостов
         ip = user_dialog.get_subnet_from_user()
-        network_scanner = NetworkScanner(ip)
-        network_servers = network_scanner.scan(user_dialog)
     else:
-        login = arg_parse.login
-        password = arg_parse.password
+        user_dialog.username = arg_parse.login
+        user_dialog.password = arg_parse.password
         ip = arg_parse.subnet
+        
+    # Сканируем сеть на наличие хостов
+    network_scanner = NetworkScanner(ip)
+    network_servers = network_scanner.scan(user_dialog)
 
     # Записываем экземпляры класса SSHClient в лист servers
     servers = []
